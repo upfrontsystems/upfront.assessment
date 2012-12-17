8df613477b46cf6ae51408f65085fc7a74f8e60e
witek <witekdev@gmail.com>
2012-12-17 13:01:44 +0200

    add assessment_items relationlist to assessment's schema.

--- upfront/assessment/content/assessment.py
+++ upfront/assessment/content/assessment.py
@@ -1,12 +1,24 @@
 from five import grok
 
 from z3c.form.i18n import MessageFactory as _
+from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.directives import dexterity, form
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
+from upfront.assessmentitem.content.assessmentitem import IAssessmentItem
 
 class IAssessment(form.Schema):
     """ Description of Assessment content type
     """
 
+    form.omitted('assessment_items')
+    assessment_items = RelationList(
+            title=u"Assessment Items",
+            default=[],
+            value_type=RelationChoice(title=_(u"Related"),
+    source=ObjPathSourceBinder(object_provides=IAssessmentItem.__identifier__)),
+            required=False,
+        )
 
 class Assessment(dexterity.Container):
     grok.implements(IAssessment)
