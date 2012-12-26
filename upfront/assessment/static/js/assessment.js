b1974b892ab49b5135ae14f72424feaf145fe922
witek <witekdev@gmail.com>
2012-12-26 19:00:58 +0200

    move activities up and down ajax functionality.

--- upfront/assessment/static/js/assessment.js
+++ upfront/assessment/static/js/assessment.js
@@ -15,6 +15,38 @@ $(function() {
         });
     });
 
+    $(".move-up").live("click", function() {
+
+        clearErrors();
+        var id = $(this).attr('id')
+        console.log(id)
+        $.ajax({
+            url: '@@move-up-assessmentitem',
+            data: {
+                'id': id
+            },
+            dataType: "json",
+            success: updateAssessmentPostMoveUp,
+            error: displayError,
+        });
+    });
+
+    $(".move-down").live("click", function() {
+
+        clearErrors();
+        var id = $(this).attr('id')
+        console.log(id)
+        $.ajax({
+            url: '@@move-down-assessmentitem',
+            data: {
+                'id': id
+            },
+            dataType: "json",
+            success: updateAssessmentPostMoveDown,
+            error: displayError,
+        });
+    });
+
 });
 
 function updateAssessmentPostRemove(data) {
@@ -27,6 +59,69 @@ function updateAssessmentPostRemove(data) {
 
     showStatusMessage(data);
 }
+
+function updateAssessmentPostMoveUp(data) {
+
+    var activity = $('.activity-container .item-actions-left\
+        .remove-from-assessment[value=' + data.id + ']').parent().parent()
+
+    // if moving up from last position
+    if ( activity.next().size() == 0 ) {
+
+        //activity moving up needs a move-down link
+        var html ='<a id='+'"'+data.id+'"'+'class="move-down">Move down</a>'
+        $(html).insertAfter(activity.find('a.move-up'))
+
+        //remove move-down link from activity that will be in last place
+        activity.prev().find('a.move-down').remove()
+    }
+
+    // if moving up into first position, remove move-up link
+    if ( activity.prev().prev().size() == 0 ) {
+        console.log("INTO FIRST")
+        activity.find('a.move-up').remove()
+
+        //activity that is being moved-down from first needs a moveup link
+        var id = activity.prev().find('a.move-down').attr('id')
+        var html ='<a id='+'"'+id+'"'+'class="move-up">Move up</a>'
+        $(html).insertBefore(activity.prev().find('a.move-down'))
+    }
+
+    // move up
+    activity.insertBefore(activity.prev())
+}
+
+function updateAssessmentPostMoveDown(data) {
+
+    var activity = $('.activity-container .item-actions-left\
+        .remove-from-assessment[value=' + data.id + ']').parent().parent()
+
+    // if moving down from first position
+    if ( activity.prev().size() == 0 ) {
+
+        //activity moving down needs a move-up link
+        var html ='<a id='+'"'+data.id+'"'+'class="move-up">Move up</a>'
+        $(html).insertBefore(activity.find('a.move-down'))
+
+        //remove move-up link from activity that will be in first place
+        activity.next().find('a.move-up').remove()
+    }
+
+    // if moving down into last position, remove move-down link
+    if ( activity.next().next().size() == 0 ) {
+        console.log("INTO LAST")
+        activity.find('a.move-down').remove()
+
+        //activity that is being moved-up from last needs a movedown link
+        var id = activity.next().find('a.move-up').attr('id')
+        var html ='<a id='+'"'+id+'"'+'class="move-down">Move down</a>'
+        $(html).insertAfter(activity.next().find('a.move-up'))
+    }
+
+    // move down
+    activity.insertAfter(activity.next())
+}
+
    
 function displayError(data) {
     var data = {'status' : 'error', 'msg' : 'ajax error'}
