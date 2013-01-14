c201b6c85a232392c1da79fb8d31ddb29ca6d29b
witek <witekdev@gmail.com>
2013-01-14 19:00:20 +0200

    add export evaluationsheets browser view.

---
+++ upfront/assessment/browser/exportevaluationsheets.py
@@ -0,0 +1,114 @@
+from csv import DictWriter
+from cStringIO import StringIO
+from DateTime import DateTime
+
+from five import grok
+from zope.component.hooks import getSite
+from zope.interface import Interface
+
+from Products.statusmessages.interfaces import IStatusMessage
+
+from upfront.assessment import MessageFactory as _
+
+class ExportEvaluationSheetsView(grok.View):
+    """ Export of all evaluation sheets completed between a given date range. 
+        The export has following columns: 
+        Assessment, Date of Assessment, Class, Learner, Activity Number, Rating
+    """
+    grok.context(Interface)
+    grok.name('export-evaluationsheets')
+    grok.require('zope2.View')
+
+    def __call__(self):
+        """ Export of all evaluation sheets completed between a given date range
+            The export has following columns:   Assessment, Date of Assessment,
+            Class, Learner, Activity Number, Rating
+
+        """
+
+        # get optional parameters off the request - in epoch format
+        start_date = self.request.get('start_date', '')
+        end_date = self.request.get('end_date', '')
+
+        if start_date != '' and end_date != '':
+            start = int(start_date)
+            end = int(end_date)
+        else:
+            # if no dates given - display all evaluation sheets ever completed
+            start = DateTime('2013-01-01 00:00:00')
+            end = DateTime() # now
+
+        date_range_query = { 'query':(start,end), 'range': 'min:max'}   
+        evalsheets = getSite().portal_catalog(
+                   portal_type = ['upfront.assessment.content.evaluationsheet'],
+                   effective = date_range_query)
+
+        csv_content = None
+        evalsheet_csv = StringIO()
+
+        if evalsheets is not None and len(evalsheets) > 0:
+            writer = DictWriter(evalsheet_csv,
+                                fieldnames=['assessment', 'assessment_date',
+                                            'class','learner','activity_number',
+                                            'rating'],
+                                restval='',
+                                extrasaction='ignore',
+                                dialect='excel'
+                               )
+
+            for brain in evalsheets:
+                evalsheet = brain.getObject()
+                contentFilter =\
+                        {'portal_type': 'upfront.assessment.content.evaluation'}
+                folder_contents = evalsheet.getFolderContents(contentFilter)
+                for idx, evaluation in enumerate(folder_contents):
+                    e_obj = evaluation.getObject()
+
+                    # for each evaluation - go through all its activities
+                    for activity in range(len(e_obj.evaluation)):
+                        rating = e_obj.evaluation[activity]['rating']
+                        if rating == -1:
+                            rating = _(u'Not Rated')
+
+                        ldict={'assessment': evalsheet.assessment.to_object.id,
+                               'assessment_date': evalsheet.assessment.\
+                                    to_object.created().strftime('%d %B %Y'),
+                               'class': evalsheet.classlist.to_object.id,
+                               'learner': e_obj.learner.to_object.name,
+                               'activity_number': activity+1, # count from 1 on
+                               'rating': rating, # a number or 'Not Rated'
+                          }
+                        writer.writerow(ldict)
+            
+            csv_content = evalsheet_csv.getvalue()
+            evalsheet_csv.close()
+
+            now = DateTime()
+            nice_filename = '%s_%s' % ('completed_evaluationsheets_',
+                             now.strftime('%Y%m%d'))
+            self.request.response.setHeader("Content-Disposition",
+                                            "attachment; filename=%s.csv" % 
+                                             nice_filename)
+            self.request.response.setHeader("Content-Type", "text/csv")
+            self.request.response.setHeader("Content-Length", len(csv_content))
+            self.request.response.setHeader('Last-Modified',
+                                            DateTime.rfc822(DateTime()))
+            self.request.response.setHeader("Cache-Control", "no-store")
+            self.request.response.setHeader("Pragma", "no-cache")
+
+            self.request.response.write(csv_content)
+
+        else:
+            msg = _('No completed evaluationsheets exist')
+            IStatusMessage(self.request).addStatusMessage(msg,"info")
+
+        # redirect to show the info message
+        self.request.response.redirect(
+                '/'.join(self.context.getPhysicalPath()))
+
+        return csv_content
+
+    def render(self):
+        """ No-op to keep grok.View happy
+        """
+        return ''
