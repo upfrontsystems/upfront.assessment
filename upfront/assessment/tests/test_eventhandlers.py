3484785a2de015b44dc08ad3b75fab37fcdd2709
witek <witekdev@gmail.com>
2013-03-25 14:54:07 +0200

    add and update eventhandler unit tests.

--- upfront/assessment/tests/test_eventhandlers.py
+++ upfront/assessment/tests/test_eventhandlers.py
@@ -1,17 +1,25 @@
 from zope.event import notify
 from zope.lifecycleevent import ObjectModifiedEvent
 from zope.app.component.hooks import getSite
+from z3c.relationfield import RelationValue
+
 from plone.uuid.interfaces import IUUID
 from Products.CMFCore.utils import getToolByName
 
 from base import UpfrontAssessmentTestBase
 from upfront.assessment.eventhandlers import on_evaluationsheet_created,\
-                                             on_evaluation_modified
+                                             on_evaluation_modified,\
+                                             on_evaluationsheet_deleted
 
 class TestEventhandlers(UpfrontAssessmentTestBase):
     """ Test event handlers """
     
     def test_on_evaluationsheet_created(self):
+
+        pw = getSite().portal_workflow
+        assessment = self.evaluationsheet1.assessment.to_object
+        state = pw.getStatusOf('assessment_workflow',assessment)['state']
+        self.assertEquals(state,'frozen')
 
         cF = {'portal_type': 'upfront.assessment.content.evaluation'}
         self.evaluationsheet1.getFolderContents(cF)
@@ -33,7 +41,43 @@ class TestEventhandlers(UpfrontAssessmentTestBase):
         self.assertEqual(evaluations[0].getObject().evaluation[1]['uid'],
                          IUUID(self.activity2))
         self.assertEqual(evaluations[0].getObject().evaluation[2]['uid'],
-                         IUUID(self.activity3))    
+                         IUUID(self.activity3))
+
+    def test_on_evaluationsheet_deleted(self):
+
+        # create new evaluationsheet and link to new assessment
+        eval_factory = 'upfront.assessment.content.evaluationsheet'
+        self.evaluationsheets.invokeFactory(eval_factory,
+                                      'evalsheet3', title='EvalSheet3')
+        self.evaluationsheet3 = self.evaluationsheets._getOb('evalsheet3')
+
+        self.assessments.invokeFactory('upfront.assessment.content.assessment',
+                                      'test3', title='Test3')
+        self.assessment3 = self.assessments._getOb('test3')
+
+        classlist1_intid = self.intids.getId(self.classlist1)
+        self.evaluationsheet3.classlist = RelationValue(classlist1_intid)
+        assessment3_intid = self.intids.getId(self.assessment3)
+        self.evaluationsheet3.assessment = RelationValue(assessment3_intid)
+
+        pw = getSite().portal_workflow
+        state = pw.getStatusOf('assessment_workflow',self.assessment3)['state']
+        self.assertEquals(state,'editable')
+        notify(ObjectModifiedEvent(self.evaluationsheet3))
+
+        on_evaluationsheet_created(self.evaluationsheet3, None)
+
+        state = pw.getStatusOf('assessment_workflow',self.assessment3)['state']
+        self.assertEquals(state,'frozen')
+
+        # delete self.evaluationsheet3
+        parent = self.evaluationsheet3.aq_parent
+        parent.manage_delObjects(self.evaluationsheet3.getId())
+
+        on_evaluationsheet_deleted(self.evaluationsheet3, None)
+
+        state = pw.getStatusOf('assessment_workflow',self.assessment3)['state']
+        self.assertEquals(state,'editable')
 
     def test_on_evaluation_modified(self):
 
